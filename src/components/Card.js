2d79473291753dfc1ee0f74162a7b6084ad2f6aa
AbdElMajidOuhamane <abdelmajidouhamne01@gmail.com>
2023-03-19 00:10:12 +0100

    still fixing

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+
 
 const Card = ({path,title,createdAt}) => {
+  const timestamp=useMemo(() => {
+    const date= `${new Date(createdAt.seconds*1000)}`.split(' ');
+    return `${date[1] } ${date[2]} ${date[3]}`
+  }
+  , [])
   return (
      <div className='col mb-5'>
             <div className="card" style={{width:"18rem"}}>
-                  <img src={path} className="card-img-top" alt={title} />
-                </div>
+            <div style={{
+              height:"200px",
+              backgroundImage:`url(${path})`,
+              backgroundSize:"cover",
+              backgroundRepeat:"no-repeat"
+            }}>
+
+
+            </div>
+            <h5 className='text-center mt-1'>{title}</h5>
+            <div className='d-flex justify-content-between p-2'>
+            <p>{timestamp}</p>
+            <p>@username</p>
+            </div>
+                  {/* <img src={path} className="card-img-top" alt={title} /> */}
+            </div>
           </div>
   )
 }
